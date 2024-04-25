b13aca81b2b2b88fdb81a995c813387888429d76
Nishant Goyal <nishant@pluralcode.com>
2024-04-25 13:43:57 +0530

    comma string case spec

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,10 +5,18 @@ def add_to(number_string)
   number_string.extend(StringCalculator).add
 end
 
+def add_to_error_message(number_string, expected_sum)
+
+end
+
 RSpec.describe "StringCalculator" do
   context "sum method" do
     it "should return 0 for empty string" do
       expect(add_to("")).to eq(0)
+    end
+
+    it "should add correctly if numbers in input are comma separated" do
+      expect(add_to("2,3")).to eq(5)
     end
   end
 end
